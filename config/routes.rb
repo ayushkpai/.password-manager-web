f98845e336753ed965c91e3c2cbe8b60a7986aca
Ayush Pai <ayushpai@ayushpai.com>
2026-05-31 18:31:45 +0530

    remove form if signed in

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
+  root "passwords#index"
   get "up" => "rails/health#show", as: :rails_health_check
-
-  root "sessions#new"
 
   get "/signup", to: "users#new"
   post "/signup", to: "users#create"
-
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
-
   delete "/logout", to: "sessions#destroy"
 
   get "/welcome", to: "sessions#welcome"
