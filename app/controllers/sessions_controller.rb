1bf34b1a1ae866382ce60d6aac5633b9b0bc521d
Ayush Pai <ayushpai@ayushpai.com>
2026-05-31 17:47:58 +0530

    format

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
 
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to passwords_path
+      redirect_to welcome_path
     else
       render :new, status: :unprocessable_entity
     end
